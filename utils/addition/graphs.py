0aeddc09d4166fb2671506d934051e40597ae8be
ramakanth1997 <115607699+ramakanth1997@users.noreply.github.com>
2023-07-21 10:36:26 +0530

    Add files via upload

---
+++ utils/addition/graphs.py
@@ -0,0 +1,59 @@
+from streamlit_elements import elements, mui
+from streamlit_elements import nivo
+
+def graph_pes(DATA):
+        with elements("nivo_charts"):        
+            with mui.Box(sx={"height": 400}):
+                nivo.Radar(
+                    data=DATA,
+                    keys=["Peso Area"],
+                    indexBy="taste",
+                    maxValue=7,
+                    valueFormat=">-.2f",
+                    margin={"top": 80, "right": 60, "bottom": 80, "left": 60},
+                    gridLabelOffset=36,
+                    dotSize=10,
+                    dotColor={"theme": "background"},
+                    dotBorderWidth=1,
+                    fillOpacity=0.85,
+                    borderWidth=2,
+                    borderColor="#e08367",
+                    dotBorderColor="#e08367",
+                    motionConfig="wobbly",
+                    legends=[
+                        {
+                            "anchor": "top-left",
+                            "direction": "column",
+                            "translateX": -70,
+                            "translateY": -100,
+                            "itemWidth": 40,
+                            "itemHeight": 20,
+                            "itemTextColor": "#999",
+                            "symbolSize": 12,
+                            "symbolShape": "circle",
+                            "effects": [
+                                {
+                                    "on": "hover",
+                                    "style": {
+                                        "itemTextColor": "#000"
+                                    }
+                                }
+                            ]
+                        }
+                    ],
+                    theme={
+                        "background": "#E4E3E3",
+                        "textColor": "#31333F",
+                        "grid": {"line": {
+                                            "stroke": "#b3bcc4",
+                                            "strokeWidth": 1
+                                        }
+                                    },
+                        "tooltip": {
+                            "container": {
+                                "background": "#E4E3E3",
+                                "color": "#31333F",
+                            }
+                        }
+                    }
+                )
